10fc6013e0b5c613fe90f03b08e865bd2edf26a5
helder@tretas.org <helder@tretas.org@b75609b8-96c3-ede8-46e6-d96a438264e7>
2012-06-23 03:02:15 +0000

    Used another method to number the table rows.

--- lib/bmdjango/meter/views.py
+++ lib/bmdjango/meter/views.py
@@ -6,7 +6,7 @@ from django.conf import settings
 from django.shortcuts import render_to_response
 from django.template import RequestContext
 from django.db.models import Max
-from django.core.paginator import Paginator
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 
 from meter.models import Blog, Stats
 
@@ -15,29 +15,18 @@ PAGE_DISPLAY = 25
 def result(request, dt):
     context = {}
 
-    page = request.GET.get('page', 1)
-    try:
-        page = int(page)
-    except:
-        page = 1
-
     objects = Stats.objects.filter(date__exact = dt ).order_by('-visits_daily_average')
     paginator = Paginator(objects, PAGE_DISPLAY)
 
-    if page < 1:
-        page = 1
-    if page > paginator.num_pages:
-        page = paginator.num_pages
+    page = request.GET.get('page', 1)
+    try:
+        stats = paginator.page(page)
+    except PageNotAnInteger:
+        stats = paginator.page(1)
+    except EmptyPage:
+        stats = paginator.page(paginator.num_pages)
 
-    context['page'] = paginator.page(page)
-
-    object_list = []
-    k = (page - 1)*PAGE_DISPLAY + 1
-    for obj in context['page'].object_list:
-        obj.pos = k
-        k += 1
-        object_list.append(obj)
-    context['page'].object_list = object_list
+    context['page'] = stats 
 
     return render_to_response('results.html', context,
                 context_instance=RequestContext(request))
