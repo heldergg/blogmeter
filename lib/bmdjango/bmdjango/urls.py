34c8f48a2d9289751166ef9110779ef7a3dea4cf
helder@tretas.org <helder@tretas.org@b75609b8-96c3-ede8-46e6-d96a438264e7>
2012-06-26 17:50:20 +0000

    About page.

--- lib/bmdjango/bmdjango/urls.py
+++ lib/bmdjango/bmdjango/urls.py
@@ -1,10 +1,15 @@
 # -*- coding: utf-8 -*-
 
 from django.conf.urls import patterns, include, url
+from django.views.generic import TemplateView
 
 urlpatterns = patterns('',
+    # The index view
     url(r'^$', 'meter.views.current_result', name='current_result'),
 
     # Blogmeter specific:
     (r'^mt/', include('meter.urls')),
+
+    # AboutView
+    url(r'^about/', TemplateView.as_view(template_name="about.html"), name='about'),
 )
