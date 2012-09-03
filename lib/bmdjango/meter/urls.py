ffe58304c06865968cb2001ea67810f4226e6461
helder@tretas.org <helder@tretas.org@b75609b8-96c3-ede8-46e6-d96a438264e7>
2012-09-03 20:16:22 +0000

    Blog highlighting to easily find the blog's classification.

--- lib/bmdjango/meter/urls.py
+++ lib/bmdjango/meter/urls.py
@@ -5,6 +5,9 @@ from django.conf.urls import patterns, url
 urlpatterns = patterns('meter.views',
     # Archive access
     url(r'^(?P<dt>\d{4}-\d{2}-\d{2})/$', 'result_archive', name='result_archive' ),
+    # Blog Highlight
+    url(r'^highlight/(?P<dt>\d{4}-\d{2}-\d{2})/(?P<blog_id>\d+)/$', 'blog_highlight', name='blog_highlight' ),
+    url(r'^highlight/(?P<blog_id>\d+)/$', 'highlight_today', name='highlight_today' ),
     # Blog search
     url(r'^procurar/$', 'blog_search', name='blog_search' ),
     # Blog info
--- lib/bmdjango/meter/views.py
+++ lib/bmdjango/meter/views.py
@@ -9,8 +9,9 @@ from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from django.db.models import Max
 from django.db.models import Q
 from django.http import Http404
-from django.shortcuts import render_to_response
+from django.shortcuts import render_to_response, redirect, get_object_or_404
 from django.template import RequestContext
+from django.core.urlresolvers import reverse
 
 from meter.models import Blog, Stats
 
@@ -30,7 +31,13 @@ def result(request, dt):
         '-pages_total')
     paginator = Paginator(objects, PAGE_DISPLAY)
 
+    # GET arguments
     page = request.GET.get('page', 1)
+    try:
+        highlight = int(request.GET.get('highlight', -1))
+    except:
+        highlight = -1
+
     try:
         stats = paginator.page(page)
     except PageNotAnInteger:
@@ -38,6 +45,7 @@ def result(request, dt):
     except EmptyPage:
         stats = paginator.page(paginator.num_pages)
 
+    context['highlight'] = highlight
     context['page'] = stats 
     context['update_time'] = settings.CHANGE_DAY
     context['today'] = dt
@@ -62,6 +70,52 @@ def result_archive(request, dt):
         raise Http404
     return result(request, dt)
 
+def blog_highlight(request, dt, blog_id):                
+    context = {}
+
+    # Check the URL:
+    try:
+        dt =  datetime.strptime(dt,'%Y-%m-%d').date()
+    except ValueError:
+        raise Http404
+
+    blog = get_object_or_404(Blog, id = blog_id )
+        
+    # Get the blog classification
+    objects = Stats.objects.filter(date__exact = dt ).order_by(
+        '-visits_daily_average',
+        '-visits_total',
+        '-pages_daily_average',
+        '-pages_total')
+
+    # TODO: This is awfully inefficient. We must find a way to do this properly
+    # maybe by maintaining a classification table or column in Stats.
+    have_classification = False
+    for index, stat in enumerate(objects):    
+        if stat.blog.id == blog.id:
+            have_classification = True
+            break
+
+    if not have_classification:
+        context['blog'] = blog
+        context['date'] = dt
+        return render_to_response('no_classification.html', context,
+               context_instance=RequestContext(request))
+      
+    # Calculate the page number:
+    page_number = index / PAGE_DISPLAY + 1 
+
+    return redirect('%s?page=%d&highlight=%d' % (reverse('result_archive', kwargs={'dt':dt}),
+                                    page_number,
+                                    blog.id))
+
+def highlight_today(request, blog_id):
+    blog = get_object_or_404(Blog, id = blog_id )
+    if settings.CHANGE_DAY > datetime.now().time():
+        # Show yesterday data:
+        return blog_highlight(request, (datetime.now() - timedelta(1)).date().isoformat(), blog.id )
+    else:    
+        return blog_highlight(request, date.today().isoformat(), blog.id )
 ##
 ## Blog search
 ##
@@ -98,10 +152,7 @@ def blog_search(request):
 def blog_info(request, blog_id):
     context = {}
 
-    try:
-        blog = Blog.objects.get( id = blog_id )
-    except ObjectDoesNotExist:
-        raise Http404
+    blog = get_object_or_404(Blog, id = blog_id )
         
     objects = Stats.objects.filter(blog__exact = blog ).order_by( '-date' )
     paginator = Paginator(objects, PAGE_DISPLAY)
@@ -119,3 +170,6 @@ def blog_info(request, blog_id):
 
     return render_to_response('blog_info.html', context,
                 context_instance=RequestContext(request))
+
+           
+           
